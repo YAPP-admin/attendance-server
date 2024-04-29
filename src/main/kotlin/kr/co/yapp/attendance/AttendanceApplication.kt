88427fc24e65f50b44ad60a269ebc672b858d4a7
junhaesung <seong0428@gmail.com>
2024-04-29 12:32:30 +0900

    Initial commit

---
+++ src/main/kotlin/kr/co/yapp/attendance/AttendanceApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.yapp.attendance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AttendanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AttendanceApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/yapp/attendance/AttendanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.yapp.attendance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
